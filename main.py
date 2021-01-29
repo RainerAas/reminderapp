ef0c612ff95ca8eb5c3c588d5f1e26896072509f
Rainer <raineraas2@gmail.com>
2021-01-29 15:53:43 +0200

    V1

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+"""
+Reminder app.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+By: Rainer Aas
+Contact: raineraas2@gmail.com
+"""
+
+import winsound
+from win10toast import ToastNotifier
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def timer(reminder, minutes):
+    """Timer function."""
+    seconds = minutes * 60
+    notificator = ToastNotifier()
+
+    if minutes < 1:
+        text = f"The alarm will go off in {seconds} seconds."
+    elif minutes == 1:
+        x_minutes = int(minutes)
+        text = f"The alarm will go off in {x_minutes} minute."
+    else:
+        text = f"The alarm will go off in {minutes} minutes."
+
+    notificator.show_toast("Reminder", text, duration=seconds)
+    notificator.show_toast("Reminder", reminder, duration=0)
+
+    for i in range(6):
+        frequency = 900
+        duration = 100
+        winsound.Beep(frequency, duration)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    reminder_text = str(input("What would you like a reminder about? "))
+    is_correct = False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    try:
+        minutes = float(input("In how many minutes? "))
+        is_correct = True
+    except ValueError:
+        print("Please enter a valid number!")
+
+    if is_correct:
+        timer(reminder_text, minutes)
